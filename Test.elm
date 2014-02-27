9ccbc88d25f28badc077eb7eae8549a5dd05c214
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-02-27 23:23:29 +0800

    Remove dead code

--- Test.elm
+++ Test.elm
@@ -11,6 +11,3 @@ tests = [ (2^3) ~=? 1
         , defaultTest (assert True)
         , test "test head" (assertEqual 1 (head [1..10]))
         ]
-       -- (testFunction "Square"
-       --                                 (\x -> if (x < 4) then x^2 else x^3)
-       --                                 (zip [1..5] [1,4,9,16,25]))
