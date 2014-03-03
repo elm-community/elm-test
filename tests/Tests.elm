37244a402bc97627d7675c330ee7c2a0df71d47c
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-03-03 23:44:29 +0800

    Add basic compilation/equality tests
    This build (and future builds) should pass

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -2,10 +2,13 @@ module Main where
 
 import ElmTest.Runner.Console (runDisplay)
 import open ElmTest.Test
+import ElmTest.Run as R
+import ElmTest.Assertion as A
 
 tests : [Test]
-tests = [ 5 `equals` 5
-        , 10 `equals` 9
-        ]
+tests = [ (R.run (0 `equals` 0)) `equals` Nothing
+        , test "pass" <| A.assert (R.pass Nothing)
+        , test "fail" <| A.assertNotEqual (R.fail Nothing) True
+        ] ++ (map defaultTest <| A.assertionList [1..10] [1..10])
 
 console = runDisplay tests
