b86a34757261790eebb4ceefd793beaf60bddb2c
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-04-05 20:14:59 +0800

    Update library test cases

--- Tests/Tests.elm
+++ Tests/Tests.elm
@@ -6,9 +6,9 @@ import ElmTest.Runner.Console (runDisplay)
 import ElmTest.Test (..)
 
 tests : [Test]
-tests = [ (R.run (0 `equals` 0)) `equals` R.Pass
-        , test "pass" <| A.assert (R.pass R.Pass)
-        , test "fail" <| A.assertNotEqual (R.fail R.Pass) True
+tests = [ (R.run (0 `equals` 0)) `equals` (R.Pass "0 == 0")
+        , test "pass" <| A.assert (R.pass <| R.Pass "")
+        , test "fail" <| A.assertNotEqual (R.fail <| R.Pass "") True
         ] ++ (map defaultTest <| A.assertionList [1..10] [1..10])
 
 console = runDisplay <| Suite "All Tests" tests
