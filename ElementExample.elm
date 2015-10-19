44ffdaec34a2533162e9fd7148e82ea617af5a66
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-10-19 22:16:44 +0800

    Update code style

--- ElementExample.elm
+++ ElementExample.elm
@@ -7,23 +7,34 @@ import ElmTest.Run as Run
 import ElmTest.Runner.Element as ElementRunner
 import ElmTest.Runner.String  as StringRunner
 
+
 plainText : String -> Element
-plainText s = leftAligned (Text.fromString s)
+plainText s = 
+    leftAligned (Text.fromString s)
+
 
 prettyOut : Element
-prettyOut = ElementRunner.runDisplay Test.suite3
+prettyOut = 
+    ElementRunner.runDisplay Test.suite3
+
 
 uglyOut : String
-uglyOut = StringRunner.runDisplay Test.suite2
+uglyOut = 
+    StringRunner.runDisplay Test.suite2
+
 
 uglyOut' : String
-uglyOut' = StringRunner.runDisplay Test.suite3
+uglyOut' = 
+    StringRunner.runDisplay Test.suite3
+
 
 main : Element
-main = flow down [ plainText "All tests in this suite will pass:\n\n"
-                 , plainText uglyOut
-                 , plainText "\nThis suite has a failing test:\n\n"
-                 , plainText uglyOut'
-                 , plainText "\nThe Element runner:\n\n"
-                 , prettyOut
-                 ]
+main = 
+    flow down 
+        [ plainText "All tests in this suite will pass:\n\n"
+        , plainText uglyOut
+        , plainText "\nThis suite has a failing test:\n\n"
+        , plainText uglyOut'
+        , plainText "\nThe Element runner:\n\n"
+        , prettyOut
+        ]
