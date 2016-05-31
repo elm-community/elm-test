50dd1bd44d4bbd4e1e8a2c740fe063cb8b220128
Max Goldstein <maxgoldstein1@gmail.com>
2016-05-30 22:28:04 -0700

    Run everything through elm-format

--- src/Example.elm
+++ src/Example.elm
@@ -8,33 +8,32 @@ import Html
 {-| A fuzzzer that usually generates "foo", but occasonally "bar". We expect a claim that it's always "foo" to fail.
 -}
 usuallyFoo =
-  Fuzzer
-    (Random.oneIn 30
-      |> Random.map
-          (\b ->
-            if b then
-              "bar"
-            else
-              "foo"
-          )
-    )
+    Fuzzer
+        (Random.oneIn 30
+            |> Random.map
+                (\b ->
+                    if b then
+                        "bar"
+                    else
+                        "foo"
+                )
+        )
 
 
 actualFuzzSuite : Test
 actualFuzzSuite =
-  Test.fuzz
-    usuallyFoo
-    [ \shouldBeFoo ->
-        { expected = "foo"
-        , actually = shouldBeFoo
-        }
-          |> assertEqual
-          |> onFail "It wasn't \"foo\"."
-    ]
+    Test.fuzz usuallyFoo
+        [ \shouldBeFoo ->
+            { expected = "foo"
+            , actually = shouldBeFoo
+            }
+                |> assertEqual
+                |> onFail "It wasn't \"foo\"."
+        ]
 
 
 main =
-  Html.text (toString <| runWithSeed (Random.initialSeed 42) actualFuzzSuite)
+    Html.text (toString <| runWithSeed (Random.initialSeed 42) actualFuzzSuite)
 
 
 
@@ -44,81 +43,80 @@ main =
 {-| stubbed function under test
 -}
 oxfordify _ _ _ =
-  "Alice, Bob, and Claire"
+    "Alice, Bob, and Claire"
 
 
 {-| Stubbed fuzzer
 -}
 string : Fuzzer String
 string =
-  Fuzzer
-    <| Random.map
-        (\b ->
-          if b then
-            "foo"
-          else
-            "bar"
-        )
-        Random.bool
+    Fuzzer
+        <| Random.map
+            (\b ->
+                if b then
+                    "foo"
+                else
+                    "bar"
+            )
+            Random.bool
 
 
 fuzzSuite : Test
 fuzzSuite =
-  Test.fuzz2
-    string
-    string
-    [ \name punctuation ->
-        { expected = ""
-        , actually = oxfordify "This sentence is empty" "." []
-        }
-          |> assertEqual
-          |> onFail "given an empty list, did not return an empty string"
-    , \name punctuation ->
-        { expected = "This sentence contains one item."
-        , actually = oxfordify "This sentence contains " "." [ "one item" ]
-        }
-          |> assertEqual
-    , \name punctuation ->
-        { expected = "This sentence contains one item and two item."
-        , actually = oxfordify "This sentence contains " "." [ "one item", "two item" ]
-        }
-          |> assertEqual
-          |> onFail "given an empty list, did not return an empty string"
-    , \name punctuation ->
-        { expected = "This sentence contains one item, two item, and three item."
-        , actually = oxfordify "This sentence contains " "." [ "one item", "two item", "three item" ]
-        }
-          |> assertEqual
-          |> onFail "given a list of length 3, did not return an oxford-style sentence"
-    ]
-    |> onFail "oxfordify failed"
+    Test.fuzz2 string
+        string
+        [ \name punctuation ->
+            { expected = ""
+            , actually = oxfordify "This sentence is empty" "." []
+            }
+                |> assertEqual
+                |> onFail "given an empty list, did not return an empty string"
+        , \name punctuation ->
+            { expected = "This sentence contains one item."
+            , actually = oxfordify "This sentence contains " "." [ "one item" ]
+            }
+                |> assertEqual
+        , \name punctuation ->
+            { expected = "This sentence contains one item and two item."
+            , actually = oxfordify "This sentence contains " "." [ "one item", "two item" ]
+            }
+                |> assertEqual
+                |> onFail "given an empty list, did not return an empty string"
+        , \name punctuation ->
+            { expected = "This sentence contains one item, two item, and three item."
+            , actually = oxfordify "This sentence contains " "." [ "one item", "two item", "three item" ]
+            }
+                |> assertEqual
+                |> onFail "given a list of length 3, did not return an oxford-style sentence"
+        ]
+        |> onFail "oxfordify failed"
 
 
 oxfordifySuite : Test
 oxfordifySuite =
-  Test.unit
-    [ \_ ->
-        { expected = ""
-        , actually = oxfordify "This sentence is empty" "." []
-        }
-          |> assertEqual
-          |> onFail "given an empty list, did not return an empty string"
-    , \_ ->
-        { expected = "This sentence contains one item."
-        , actually = oxfordify "This sentence contains " "." [ "one item" ]
-        }
-          |> assertEqual
-    , \_ ->
-        { expected = "This sentence contains one item and two item."
-        , actually = oxfordify "This sentence contains " "." [ "one item", "two item" ]
-        }
-          |> assertEqual
-          |> onFail "given an empty list, did not return an empty string"
-    , \_ ->
-        { expected = "This sentence contains one item, two item, and three item."
-        , actually = oxfordify "This sentence contains " "." [ "one item", "two item", "three item" ]
-        }
-          |> assertEqual
-          |> onFail "given a list of length 3, did not return an oxford-style sentence"
-    ]
-    |> onFail "oxfordify failed"
+    Test.unit
+        [ \_ ->
+            { expected = ""
+            , actually = oxfordify "This sentence is empty" "." []
+            }
+                |> assertEqual
+                |> onFail "given an empty list, did not return an empty string"
+        , \_ ->
+            { expected = "This sentence contains one item."
+            , actually = oxfordify "This sentence contains " "." [ "one item" ]
+            }
+                |> assertEqual
+        , \_ ->
+            { expected = "This sentence contains one item and two item."
+            , actually = oxfordify "This sentence contains " "." [ "one item", "two item" ]
+            }
+                |> assertEqual
+                |> onFail "given an empty list, did not return an empty string"
+        , \_ ->
+            { expected = "This sentence contains one item, two item, and three item."
+            , actually = oxfordify "This sentence contains " "." [ "one item", "two item", "three item" ]
+            }
+                |> assertEqual
+                |> onFail "given a list of length 3, did not return an oxford-style sentence"
+        ]
+        |> onFail "oxfordify failed"
--- src/Test.elm
+++ src/Test.elm
@@ -7,7 +7,7 @@ import Random.Pcg as Random
 
 
 type alias Outcome =
-  List ( String, String )
+    List ( String, String )
 
 
 {-| A TestTree is either
@@ -17,151 +17,148 @@ type alias Outcome =
    * A node whose children are already evaluated
 -}
 type TestTree
-  = Thunk (() -> Outcome)
-  | Group (List (() -> Test))
-  | FuzzGroup (List (( Random.Seed, Int, Bool ) -> Test))
-    -- seed, runs, doShrink; make record?
-  | Batch (List Test)
+    = Thunk (() -> Outcome)
+    | Group (List (() -> Test))
+    | FuzzGroup (List (( Random.Seed, Int, Bool ) -> Test))
+      -- seed, runs, doShrink; make record?
+    | Batch (List Test)
 
 
 type alias Options =
-  { onFail : String
-  , runs : Int
-  , doShrink : Bool
-  }
+    { onFail : String
+    , runs : Int
+    , doShrink : Bool
+    }
 
 
 type Test
-  = Test Options TestTree
+    = Test Options TestTree
 
 
 type Fuzzer a
-  = -- TODO: shrinking
-    Fuzzer (Random.Generator a)
+    = -- TODO: shrinking
+      Fuzzer (Random.Generator a)
 
 
 batch : List Test -> Test
 batch tests =
-  Test { onFail = "Batch failed:", runs = 1, doShrink = False } (Batch tests)
+    Test { onFail = "Batch failed:", runs = 1, doShrink = False } (Batch tests)
 
 
 onFail : String -> Test -> Test
 onFail str (Test opts tree) =
-  Test { opts | onFail = str } tree
+    Test { opts | onFail = str } tree
 
 
 runs : Int -> Test -> Test
 runs int (Test opts tree) =
-  Test { opts | runs = int } tree
+    Test { opts | runs = int } tree
 
 
 unit : List (() -> Test) -> Test
 unit tests =
-  Test { onFail = "Unit test suite failed:", runs = 1, doShrink = False }
-    <| Group
-    <| List.map (\t _ -> t ()) tests
+    Test { onFail = "Unit test suite failed:", runs = 1, doShrink = False }
+        <| Group
+        <| List.map (\t _ -> t ()) tests
 
 
 fuzz : Fuzzer a -> List (a -> Test) -> Test
 fuzz (Fuzzer gen) fuzzTests =
-  Test { onFail = "Fuzz test suite failed:", runs = 100, doShrink = True }
-    <| FuzzGroup
-    <| (flip List.map)
-        fuzzTests
-        (\fuzzTest ( seed, runs, doShrink ) ->
-          let
-            genTests =
-              Random.list runs gen |> Random.map (List.map (\arg _ -> fuzzTest arg))
+    Test { onFail = "Fuzz test suite failed:", runs = 100, doShrink = True }
+        <| FuzzGroup
+        <| (flip List.map) fuzzTests
+            (\fuzzTest ( seed, runs, doShrink ) ->
+                let
+                    genTests =
+                        Random.list runs gen |> Random.map (List.map (\arg _ -> fuzzTest arg))
 
-            opts =
-              { onFail = "Fuzz test failed:", runs = runs, doShrink = doShrink }
-          in
-            Random.step genTests seed |> fst |> Group |> Test opts
-        )
+                    opts =
+                        { onFail = "Fuzz test failed:", runs = runs, doShrink = doShrink }
+                in
+                    Random.step genTests seed |> fst |> Group |> Test opts
+            )
 
 
 fuzz2 : Fuzzer a -> Fuzzer b -> List (a -> b -> Test) -> Test
 fuzz2 (Fuzzer genA) (Fuzzer genB) fuzzTests =
-  Test { onFail = "Fuzz test suite failed:", runs = 100, doShrink = True }
-    <| FuzzGroup
-    <| (flip List.map)
-        fuzzTests
-        (\fuzzTest ( seed, runs, doShrink ) ->
-          let
-            genTuple =
-              Random.map2 (,) genA genB
+    Test { onFail = "Fuzz test suite failed:", runs = 100, doShrink = True }
+        <| FuzzGroup
+        <| (flip List.map) fuzzTests
+            (\fuzzTest ( seed, runs, doShrink ) ->
+                let
+                    genTuple =
+                        Random.map2 (,) genA genB
 
-            genTests =
-              Random.list runs genTuple |> Random.map (List.map (\( a, b ) _ -> fuzzTest a b))
+                    genTests =
+                        Random.list runs genTuple |> Random.map (List.map (\( a, b ) _ -> fuzzTest a b))
 
-            opts =
-              { onFail = "Fuzz test failed:", runs = runs, doShrink = doShrink }
-          in
-            Random.step genTests seed |> fst |> Group |> Test opts
-        )
+                    opts =
+                        { onFail = "Fuzz test failed:", runs = runs, doShrink = doShrink }
+                in
+                    Random.step genTests seed |> fst |> Group |> Test opts
+            )
 
 
 assertEqual : { expected : a, actually : a } -> Test
 assertEqual { expected, actually } =
-  Test
-    { onFail = "Test failed:", runs = 1, doShrink = False }
-    <| Thunk
-        (\_ ->
-          if expected == actually then
-            []
-          else
-            [ ( "Expected", toString expected ), ( "Actually", toString actually ) ]
-        )
+    Test { onFail = "Test failed:", runs = 1, doShrink = False }
+        <| Thunk
+            (\_ ->
+                if expected == actually then
+                    []
+                else
+                    [ ( "Expected", toString expected ), ( "Actually", toString actually ) ]
+            )
 
 
 type ResultTree
-  = Leaf String Outcome
-  | Branch String (List ResultTree)
+    = Leaf String Outcome
+    | Branch String (List ResultTree)
 
 
 runWithSeed : Random.Seed -> Test -> ResultTree
 runWithSeed seed (Test opts tree) =
-  let
-    unfiltered =
-      case tree of
-        Thunk thunk ->
-          Leaf opts.onFail (thunk ())
+    let
+        unfiltered =
+            case tree of
+                Thunk thunk ->
+                    Leaf opts.onFail (thunk ())
 
-        Group thunks ->
-          Branch opts.onFail <| List.map (\thunk -> runWithSeed seed (thunk ())) thunks
+                Group thunks ->
+                    Branch opts.onFail <| List.map (\thunk -> runWithSeed seed (thunk ())) thunks
 
-        FuzzGroup randThunks ->
-          Random.list (List.length randThunks) Random.independentSeed
-            |> Random.map (List.map2 (\randThunk seed -> randThunk ( seed, opts.runs, opts.doShrink ) |> runWithSeed seed) randThunks)
-            |> (flip Random.step) seed
-            |> fst
-            |> Branch opts.onFail
+                FuzzGroup randThunks ->
+                    Random.list (List.length randThunks) Random.independentSeed
+                        |> Random.map (List.map2 (\randThunk seed -> randThunk ( seed, opts.runs, opts.doShrink ) |> runWithSeed seed) randThunks)
+                        |> (flip Random.step) seed
+                        |> fst
+                        |> Branch opts.onFail
 
-        Batch tests ->
-          Random.list (List.length tests) Random.independentSeed
-            |> (flip Random.step) seed
-            |> fst
-            |> List.map2 (\test seed -> runWithSeed seed test) tests
-            |> Branch opts.onFail
-  in
-    filterSuccesses unfiltered |> Maybe.withDefault (Branch "No failures" [])
+                Batch tests ->
+                    Random.list (List.length tests) Random.independentSeed
+                        |> (flip Random.step) seed
+                        |> fst
+                        |> List.map2 (\test seed -> runWithSeed seed test) tests
+                        |> Branch opts.onFail
+    in
+        filterSuccesses unfiltered |> Maybe.withDefault (Branch "No failures" [])
 
 
 filterSuccesses : ResultTree -> Maybe ResultTree
 filterSuccesses rt =
-  case rt of
-    Leaf _ xs ->
-      if List.isEmpty xs then
-        Nothing
-      else
-        Just rt
+    case rt of
+        Leaf _ xs ->
+            if List.isEmpty xs then
+                Nothing
+            else
+                Just rt
 
-    Branch onFail results ->
-      let
-        filtered =
-          List.filterMap filterSuccesses results
-      in
-        if List.isEmpty filtered then
-          Nothing
-        else
-          Just (Branch onFail filtered)
+        Branch onFail results ->
+            let
+                filtered =
+                    List.filterMap filterSuccesses results
+            in
+                if List.isEmpty filtered then
+                    Nothing
+                else
+                    Just (Branch onFail filtered)
