2f0b411b512ec7dc3e2896e9997fd21e53e723d3
Richard Feldman <richard.t.feldman@gmail.com>
2016-08-16 15:08:50 -0700

    Move tests into tests/Tests, remove failing one.

--- tests/Main.elm
+++ tests/Main.elm
@@ -8,14 +8,10 @@ $ node elm.js
 Note that this always uses an initial seed of 902101337, since it can't do effects.
 -}
 
-import Expect
-import Test exposing (..)
-import Fuzz exposing (..)
 import Runner.Log
 import Html.App
 import Html
-import String
-import Char
+import Tests
 
 
 main : Program Never
@@ -25,45 +21,4 @@ main =
         , update = \_ _ -> ()
         , view = \() -> Html.text "Check the console for useful output!"
         }
-        |> Runner.Log.run (Test.concat [ readmeExample, testWithoutNums ])
-
-
-withoutNums : String -> String
-withoutNums =
-    String.filter (\ch -> not (Char.isDigit ch || ch == '.'))
-
-
-testWithoutNums : Test
-testWithoutNums =
-    describe "withoutNums"
-        [ fuzzWith { runs = 100 } (tuple3 ( string, float, string )) "adding numbers to strings has no effect" <|
-            \( prefix, num, suffix ) ->
-                withoutNums (prefix ++ toString num ++ suffix)
-                    |> Expect.equal (withoutNums (prefix ++ suffix))
-        ]
-
-
-readmeExample : Test
-readmeExample =
-    describe "The String module"
-        [ describe "String.reverse"
-            [ test "has no effect on a palindrome" <|
-                \() ->
-                    let
-                        palindrome =
-                            "hannah"
-                    in
-                        Expect.equal palindrome (String.reverse palindrome)
-            , test "reverses a known string" <|
-                \() ->
-                    "ABCDEFG"
-                        |> String.reverse
-                        |> Expect.equal "GFEDCBA"
-            , fuzz string "restores the original string if you run it again" <|
-                \randomlyGeneratedString ->
-                    randomlyGeneratedString
-                        |> String.reverse
-                        |> String.reverse
-                        |> Expect.equal randomlyGeneratedString
-            ]
-        ]
+        |> Runner.Log.run Tests.all
---
+++ tests/Tests.elm
@@ -0,0 +1,38 @@
+module Tests exposing (all)
+
+import Test exposing (..)
+import Fuzz exposing (..)
+import String
+import Expect
+
+
+all : Test
+all =
+    Test.concat
+        [ readmeExample ]
+
+
+readmeExample : Test
+readmeExample =
+    describe "The String module"
+        [ describe "String.reverse"
+            [ test "has no effect on a palindrome" <|
+                \() ->
+                    let
+                        palindrome =
+                            "hannah"
+                    in
+                        Expect.equal palindrome (String.reverse palindrome)
+            , test "reverses a known string" <|
+                \() ->
+                    "ABCDEFG"
+                        |> String.reverse
+                        |> Expect.equal "GFEDCBA"
+            , fuzz string "restores the original string if you run it again" <|
+                \randomlyGeneratedString ->
+                    randomlyGeneratedString
+                        |> String.reverse
+                        |> String.reverse
+                        |> Expect.equal randomlyGeneratedString
+            ]
+        ]
