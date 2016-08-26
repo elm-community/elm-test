e578612d58a3170c91c229b83bceeb0786bdf6f2
Max Goldstein <maxgoldstein1@gmail.com>
2016-08-25 21:04:23 -0700

    Whitebox testing to further confirm it's working

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -5,6 +5,9 @@ import Test.Expectation exposing (Expectation)
 import Fuzz exposing (..)
 import String
 import Expect
+import Fuzz.Internal
+import RoseTree
+import Random.Pcg as Random
 
 
 all : Test
@@ -79,5 +82,39 @@ fuzzerImpossible =
             "Fuzz.frequency(OrCrash)"
             (Expect.greaterThan 0)
         , fuzz (result string int) "Fuzz.result" <| \r -> Expect.pass
-        , fuzz (andThen (\i -> intRange 0 (2 ^ i)) (intRange 1 8)) "Fuzz.andThen" (Expect.atMost 256)
+        , fuzz (andThen (\i -> intRange 0 (2 ^ i)) (intRange 1 8))
+            "Fuzz.andThen"
+            (Expect.atMost 256)
+        , describe "Whitebox testing using Fuzz.Internal"
+            [ fuzz (intRange 0 0xFFFFFFFF) "the same value is generated with and without shrinking" <|
+                \i ->
+                    let
+                        seed =
+                            Random.initialSeed i
+
+                        step gen =
+                            Random.step gen seed
+
+                        aFuzzer =
+                            tuple5
+                                ( tuple ( list int, array float )
+                                , maybe bool
+                                , result unit char
+                                , tuple4
+                                    ( percentage
+                                    , map2 (+) int int
+                                    , frequencyOrCrash [ ( 1, constant True ), ( 3, constant False ) ]
+                                    , Fuzz.filter (\i -> i % 10 /= 0) int
+                                    )
+                                , tuple3 ( intRange 0 100, floatRange -51 pi, map abs int )
+                                )
+
+                        valNoShrink =
+                            aFuzzer |> Fuzz.Internal.unpackGenVal |> step |> fst
+
+                        valWithShrink =
+                            aFuzzer |> Fuzz.Internal.unpackGenTree |> step |> fst |> RoseTree.root
+                    in
+                        Expect.equal valNoShrink valWithShrink
+            ]
         ]
