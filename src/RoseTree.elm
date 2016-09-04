ee314f84b68393fcfb7ddd64cea733fd20186a15
Max Goldstein <maxgoldstein1@gmail.com>
2016-09-04 16:23:52 -0700

    Prune RoseTree of unused functions

--- src/RoseTree.elm
+++ src/RoseTree.elm
@@ -2,21 +2,8 @@ module RoseTree exposing (..)
 
 {-| RoseTree implementation in Elm using Lazy Lists.
 
-# Definition
-@docs RoseTree
-
-# Constructors
-@docs singleton
-
-# Query a rosetree
-@docs root, children, hasChildren
-
-# Common operations
-@docs addChild
-
-# Map-reduce et al.
-@docs map, map2, andMap, map3, map4, map5, zip, zip3, zip4, zip5, flatten, flatMap, andThen, reduce, sum, product
-
+This implementation is private to elm-test and has non-essential functions removed.
+If you need a complete RoseTree implementation, one can be found on elm-package.
 -}
 
 import Lazy.List as LazyList exposing (LazyList, (:::), (+++))
@@ -58,13 +45,6 @@ addChild child (Rose a c) =
     Rose a (child ::: c)
 
 
-{-| Does the tree have children?
--}
-hasChildren : RoseTree a -> Bool
-hasChildren (Rose a c) =
-    not (LazyList.isEmpty c)
-
-
 {-| Map a function over a rosetree
 -}
 map : (a -> b) -> RoseTree a -> RoseTree b
@@ -72,105 +52,8 @@ map f (Rose a c) =
     Rose (f a) (LazyList.map (map f) c)
 
 
-{-| -}
-map2 : (a -> b -> c) -> RoseTree a -> RoseTree b -> RoseTree c
-map2 f (Rose a ca) (Rose b cb) =
-    Rose (f a b) (LazyList.map2 (map2 f) ca cb)
-
-
-{-| Chain mapping operations.
--}
-andMap : RoseTree (a -> b) -> RoseTree a -> RoseTree b
-andMap =
-    map2 (<|)
-
-
-{-| -}
-map3 : (a -> b -> c -> d) -> RoseTree a -> RoseTree b -> RoseTree c -> RoseTree d
-map3 f ra rb rc =
-    f
-        `map` ra
-        `andMap` rb
-        `andMap` rc
-
-
-{-| -}
-map4 : (a -> b -> c -> d -> e) -> RoseTree a -> RoseTree b -> RoseTree c -> RoseTree d -> RoseTree e
-map4 f ra rb rc rd =
-    f
-        `map` ra
-        `andMap` rb
-        `andMap` rc
-        `andMap` rd
-
-
-{-| -}
-map5 : (a -> b -> c -> d -> e -> f) -> RoseTree a -> RoseTree b -> RoseTree c -> RoseTree d -> RoseTree e -> RoseTree f
-map5 f ra rb rc rd re =
-    f
-        `map` ra
-        `andMap` rb
-        `andMap` rc
-        `andMap` rd
-        `andMap` re
-
-
-{-| -}
-zip : RoseTree a -> RoseTree b -> RoseTree ( a, b )
-zip =
-    map2 (,)
-
-
-{-| -}
-zip3 : RoseTree a -> RoseTree b -> RoseTree c -> RoseTree ( a, b, c )
-zip3 =
-    map3 (,,)
-
-
-{-| -}
-zip4 : RoseTree a -> RoseTree b -> RoseTree c -> RoseTree d -> RoseTree ( a, b, c, d )
-zip4 =
-    map4 (,,,)
-
-
-{-| -}
-zip5 : RoseTree a -> RoseTree b -> RoseTree c -> RoseTree d -> RoseTree e -> RoseTree ( a, b, c, d, e )
-zip5 =
-    map5 (,,,,)
-
-
 {-| Flatten a rosetree of rosetrees.
 -}
 flatten : RoseTree (RoseTree a) -> RoseTree a
 flatten (Rose (Rose a c) cs) =
     Rose a (c +++ LazyList.map flatten cs)
-
-
-{-| -}
-flatMap : (a -> RoseTree b) -> RoseTree a -> RoseTree b
-flatMap f =
-    map f >> flatten
-
-
-{-| -}
-andThen : RoseTree a -> (a -> RoseTree b) -> RoseTree b
-andThen =
-    flip flatMap
-
-
-{-| -}
-reduce : (a -> b -> b) -> b -> RoseTree a -> b
-reduce reducer b (Rose a c) =
-    LazyList.reduce (flip (reduce reducer)) (reducer a b) c
-
-
-{-| -}
-sum : RoseTree number -> number
-sum =
-    reduce (+) 0
-
-
-{-| -}
-product : RoseTree number -> number
-product =
-    reduce (*) 1
